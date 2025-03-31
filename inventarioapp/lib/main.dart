bf3b8af64d005220cae31fda0f1511c68c9a5089
Rafael Braga <ralvesbraga40@gmail.com>
2025-03-31 16:48:22 -0500

    aprendendo flutter

--- inventarioapp/lib/main.dart
+++ inventarioapp/lib/main.dart
@@ -15,29 +15,86 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:
-        Scaffold(
-          appBar: AppBar(
-            title: Text('flutter demo'),
-            backgroundColor: Color(0xFF006989),
-            elevation: 0,
-            leading: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.menu,
-                    size: 30,
-                    color: Color(0xFFEAEBED)
-                ),
-            ),
-          ),
-          body: Container(
-            color: Color(0xFFEAEBED),
-            child: Center(
-              child: Text("Conteúdo Principal"),
-            ),
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tela Principal'),
         ),
+        body: ListView(
+          children: [
+            Task('Aprender Flutter'),
+            Task('Jogar'),
+            Task('Nadar'),
+            Task('Jogar'),
+            Task('Jogar'),
+            Task('Jogar'),
+            Task('Jogar'),
+            Task('Jogar'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: (){}),
+      ),
       debugShowCheckedModeBanner: false,
-      home: ConsultapPage(),
+    );
+  }
+}
+
+class Task extends StatefulWidget {
+  final String nome;
+  const Task(this.nome,{Key? key}) : super(key: key);
+
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Container(
+                        width: 200,
+                          child: Text(
+                              widget.nome,
+                              style: TextStyle(
+                                fontSize: 24,
+                                overflow: TextOverflow.ellipsis))),
+                      ElevatedButton(onPressed: (){
+                        setState(() {
+                          nivel++;
+                        });
+                        print(nivel);
+                      }, child: Icon(Icons.arrow_drop_up))
+                    ],
+                  ),
+                ),
+                Text('Level $nivel', style: TextStyle(color: Colors.white, fontSize: 16))
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ inventarioapp/lib/tela_login.dart
@@ -0,0 +1,31 @@
+// @override
+// Widget build(BuildContext context) {
+//   return MaterialApp(
+//     title: 'Flutter Demo',
+//     theme: ThemeData(
+//       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//     ),
+//     home:
+//     Scaffold(
+//       appBar: AppBar(
+//         title: Text('flutter demo'),
+//         backgroundColor: Color(0xFF006989),
+//         elevation: 0,
+//         leading: IconButton(
+//           onPressed: () {},
+//           icon: Icon(Icons.menu,
+//               size: 30,
+//               color: Color(0xFFEAEBED)
+//           ),
+//         ),
+//       ),
+//       body: Container(
+//         color: Color(0xFFEAEBED),
+//         child: Center(
+//           child: Text("Conteúdo Principal"),
+//         ),
+//       ),
+//     ),
+//     debugShowCheckedModeBanner: false,
+//   );
+// }

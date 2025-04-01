519c49f1c7ec4865b3a813f756241f0bb46e3dfe
Brenndo-l <brenndolyu3@gmail.com>
2025-04-01 18:07:11 -0500

    feat: Texto do titulo da tela de login

--- inventarioapp/lib/src/ui/pages/login_page.dart
+++ inventarioapp/lib/src/ui/pages/login_page.dart
@@ -7,19 +7,40 @@ class LoginPage extends StatelessWidget {
     Widget build(BuildContext context) {
       return Scaffold(
         backgroundColor: Colors.grey[300],
-        body: Column(children: [
-          //Logo
-          Icon(
-            Icons.lock,
-            size: 136
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: const [
+                SizedBox(height: 50),
+                //Logo
+                 Icon(
+                  Icons.lock,
+                  size: 136,
+                ), //Icon provisorio pra ver posição
+
+                SizedBox(height: 50),
+
+                //Area de titulo de Login
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 36,
+                  ), //Estilo do texto
+                ), //Texto
+
+                //Username campo
+
+                //Password campo
+
+                //sign buttomd
+              ],
+            )
+
           )
-
-          //Username campo
-
-          //Password campo
-
-          //sign buttomd
-        ])
+        )
+            
+          
 
       );
     }

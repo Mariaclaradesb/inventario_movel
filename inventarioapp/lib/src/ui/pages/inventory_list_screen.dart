68e6025712ac568c4b3e0acdca16ac150c4398ad
Brenndo-l <brenndolyu3@gmail.com>
2025-04-07 21:46:54 -0500

    feat: gambiarra 2 usando boolean pra sumir os bagui

--- inventarioapp/lib/src/ui/pages/inventory_list_screen.dart
+++ inventarioapp/lib/src/ui/pages/inventory_list_screen.dart
@@ -3,8 +3,15 @@ import 'package:inventarioapp/src/ui/widgets/app_bar.dart';
 import 'package:inventarioapp/src/ui/widgets/drawer_widgets.dart';
 import 'package:inventarioapp/src/ui/widgets/inventory_menu_add_widgets.dart';
 
-class InventoryListScreen extends StatelessWidget {
+class InventoryListScreen extends StatefulWidget {
   const InventoryListScreen({super.key});
+
+  @override
+  _InventoryListScreenState createState() => _InventoryListScreenState();
+}
+
+class _InventoryListScreenState extends State<InventoryListScreen> {
+  bool showHeader = true;
 
   @override
   Widget build(BuildContext context) {
@@ -13,116 +20,127 @@ class InventoryListScreen extends StatelessWidget {
       drawer: CustomDrawer(),
       body: ListView(
         children: [
-          Column(
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(color: Colors.blue),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            padding: EdgeInsets.all(8),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: Colors.white38,
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: Text(
-                              "10",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
+          if (showHeader)
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(color: Colors.blue),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              padding: EdgeInsets.all(8),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: Colors.white38,
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              child: Text(
+                                "10",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 32,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          flex: 4,
-                          child: Container(
-                            padding: EdgeInsets.all(8.0),
-                            decoration: BoxDecoration(
-                              color: Colors.white38,
-                              borderRadius: BorderRadius.circular(8)
-                            ),
-                            child: Column(
-                              children: [
-                                Text(
-                                  "Inventário 1",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                SizedBox(height: 4),
-                                Text(
-                                  'Data: 04/04/2025',
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            color: Colors.blue, // Fundo azul da área
-                            padding: const EdgeInsets.only(left: 16),
-                            alignment: Alignment.centerLeft,
-                            child: Material(
-                              color: Colors.transparent,
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(50), // Efeito redondo
-                                onTap: () {
-                                  showModalBottomSheet(
-                                    context: context,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                          Expanded(
+                            flex: 4,
+                            child: Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                color: Colors.white38,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Inventário 1",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
                                     ),
-                                    backgroundColor: Colors.blue[700],
-                                    builder: (context) {
-                                      return Container(
-                                        padding: const EdgeInsets.all(16),
-                                        child: Column(
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: [
-                                            ListTile(
-                                              leading: Icon(Icons.close, color: Colors.redAccent),
-                                              title: Text('Encerrar',
-                                                  style: TextStyle(color: Colors.white)),
-                                              onTap: () {
-                                                Navigator.pop(context);
-                                                print("Encerrar clicado!");
-                                              },
-                                            ),
-                                          ],
+                                  ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    'Data: 04/04/2025',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.blue,
+                              padding: const EdgeInsets.only(left: 16),
+                              alignment: Alignment.centerLeft,
+                              child: Material(
+                                color: Colors.transparent,
+                                child: InkWell(
+                                  borderRadius: BorderRadius.circular(50),
+                                  onTap: () {
+                                    showModalBottomSheet(
+                                      context: context,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.vertical(
+                                          top: Radius.circular(20),
                                         ),
-                                      );
-                                    },
-                                  );
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0), // Espaço ao redor do ícone
-                                  child: Icon(Icons.more_vert, color: Colors.white),
+                                      ),
+                                      backgroundColor: Colors.blue[700],
+                                      builder: (context) {
+                                        return Container(
+                                          padding: const EdgeInsets.all(16),
+                                          child: Column(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              ListTile(
+                                                leading: Icon(Icons.close,
+                                                    color: Colors.redAccent),
+                                                title: Text('Encerrar',
+                                                    style: TextStyle(
+                                                        color: Colors.white)),
+                                                onTap: () {
+                                                  Navigator.pop(context);
+                                                  setState(() {
+                                                    showHeader = false;
+                                                  });
+                                                  print("Encerrar clicado!");
+                                                },
+                                              ),
+                                            ],
+                                          ),
+                                        );
+                                      },
+                                    );
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Icon(
+                                      Icons.more_vert,
+                                      color: Colors.white,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                  ],
+                ),
+              ],
+            ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -130,7 +148,8 @@ class InventoryListScreen extends StatelessWidget {
           showMenuAddInventory(context);
         },
         backgroundColor: Color(0xFF006989),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100)),
         child: Icon(Icons.add, color: Colors.white),
       ),
     );

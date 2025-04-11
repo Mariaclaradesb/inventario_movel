55927360c90be64bc80073c4c921c8ab18031fbd
Mariaclaradesb <mariaclaradesb@gmail.com>
2025-04-11 20:19:18 -0300

    loginscreen

--- inventarioapp/lib/main.dart
+++ inventarioapp/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/inventoryListScreen',
+      initialRoute: '/loginScreen',
       routes: {
         '/telaInicial': (context) => TelaInicialPage(),
         '/configuracoes': (context) => ConfiguracoesPage(),

e845c26de8b6a767aa815157e6604efe6b949b74
Brenndo-l <brenndolyu3@gmail.com>
2025-09-13 16:18:39 -0500

    rapaz tentando integrar funcionario dart com o back sei se foi n

--- inventarioapp/lib/src/services/funcionario_service.dart
+++ inventarioapp/lib/src/services/funcionario_service.dart
@@ -10,20 +10,29 @@ class FuncionarioDataService {
 
   Future<List<FuncionarioData>> findAll() async {
     final int? codLoja = await SharedPrefsService.obterLojaSelecionada();
-    if(codLoja == null){
+    if (codLoja == null) {
       throw Exception("Loja não selecionada");
     }
-    
+
     String baseUrl = await ApiUrlProvider.getConfiguredUrl();
-    final uri = Uri.parse("$baseUrl/funcionarios/buscarcodLoja=$codLoja");
+    
+    
+    final uri = Uri.parse('$baseUrl/funcionarios?codLoja=$codLoja');
 
-    final response = await _apiClient.get(uri);
+    try {
+      final response = await _apiClient.get(uri);
 
-    if (response.statusCode != 200){
-      throw Exception("Erro ao buscar funcionários: ${response.body}");
+      if (response.statusCode != 200) {
+        throw Exception('Erro ao buscar funcionários: ${response.statusCode} - ${response.body}');
+      }
+
+      
+      final List<dynamic> list = json.decode(utf8.decode(response.bodyBytes));
+      
+      return list.map((f) => FuncionarioData.fromJson(f)).toList();
+    } on Exception catch (e) {
+      print('Erro ao carregar funcionários: $e');
+      rethrow; 
     }
-
-    Iterable list = json.decode(utf8.decode(response.bodyBytes));
-    return List<FuncionarioData>.from(list.map((f) => FuncionarioData.fromJson(f)));
   }
 }
